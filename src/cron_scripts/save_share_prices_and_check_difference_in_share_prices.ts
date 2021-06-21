757abfc726aec45133d780e7e5757fc065b0d8a0
Avdeev Denis <inbox@avdeevdenis.ru>
2021-06-21 07:29:13 +0000

    Fix cron launches message & correct date

--- src/cron_scripts/save_share_prices_and_check_difference_in_share_prices.ts
+++ src/cron_scripts/save_share_prices_and_check_difference_in_share_prices.ts
@@ -29,7 +29,7 @@ cron.schedule('*/5 4-20 * * 1-5', async () => {
 });
 
 export const saveCronLaunch = () => {
-  const now = DateTime.now().setZone('Europe/Moscow').toISOTime();
+  const now = DateTime.now().setZone('Europe/Moscow').toString();
   const appendData = `\n[${now}] [save_share_prices_and_check_difference_in_share_prices] launch.`;
 
   fs.appendFile('src/logs/cron_launches/save_share_prices_and_check_difference_in_share_prices.txt', appendData, (err: Error) => {
